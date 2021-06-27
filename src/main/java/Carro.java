2625a3b1a9ad97fdfe309eedcdf27a16c7e91a20
Ileis <gabrielileis07@gmail.com>
2021-06-27 11:36:37 -0300

    CarroEco

--- src/main/java/Carro.java
+++ src/main/java/Carro.java
@@ -1,39 +1,76 @@
 public class Carro {
 
+    private int tanque = 0;
+    private int passageiros = 0;
+    private int quilometragem = 0;
+
     public int getPassageiros() {
-        return -1;
+        return passageiros;
     }
 
     public int getCombustivel() {
-        return -1;
+        return tanque;
     }
 
     public int getQuilometragem() {
-        return -1;
+        return quilometragem;
     }
 
     public boolean embarcar() {
+
+        if(passageiros < 2){
+            passageiros++;
+
+            return true;
+        }
         return false;
     }
 
     public boolean desembarcar() {
+
+        if(passageiros > 0){
+            passageiros--;
+
+            return true;
+        }
         return false;
     }
 
     public boolean dirigir(int distancia) {
+
+        if((passageiros > 0) && (tanque > 0)){
+            if(distancia < tanque){
+                tanque -= distancia;
+                quilometragem += distancia;
+
+                return true;
+            }
+
+            quilometragem += tanque;
+            tanque = 0;
+        }
         return false;
     }
 
     public boolean abastecer(int quantidade) {
+
+        if((tanque < 100) && (quantidade > 0)){
+            if(quantidade > (100 - tanque))
+                tanque = 100;
+            else
+                tanque += quantidade;
+
+            return true;
+        }
         return false;
     }
 
     @Override
     public String toString() {
         return "Carro{" +
-                "passageiros=" + -1 +
-                ", combustivel=" + -1 +
-                ", quilometragem=" + -1 +
+                "passageiros=" + getPassageiros() +
+                ", combustivel=" + getCombustivel() +
+                ", quilometragem=" + getQuilometragem() +
                 '}';
     }
 }
